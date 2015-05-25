bcdf25a282c2719e31b4aef89f51e0610c0406cf
Chris <c.ljungberg90@gmail.com>
2015-05-25 12:46:29 +0200

    picking activated by default

--- Source/ECSTool/ToolPanel.cpp
+++ Source/ECSTool/ToolPanel.cpp
@@ -29,6 +29,7 @@ void MainWindow::CreateToolPanel()
 	this->togglePickingButton->Size = System::Drawing::Size(130, 20);
 	this->togglePickingButton->Location = System::Drawing::Point(270, 0);
 	this->togglePickingButton->Click += gcnew System::EventHandler(this, &MainWindow::TogglePicking_Clicked);
+	this->togglePickingButton->Checked = true;
 	this->toolPanel->Controls->Add(this->togglePickingButton);
 
 	this->togglePauseButton = (gcnew System::Windows::Forms::CheckBox());
@@ -41,7 +42,7 @@ void MainWindow::CreateToolPanel()
 
 	this->toolLabel = (gcnew System::Windows::Forms::Label());
 	this->toolPanel->Controls->Add(this->toolLabel);
-	this->toolLabel->Text = L"Tools";
+	this->toolLabel->Text = L"Tools:";
 	this->toolLabel->Location = System::Drawing::Point(0, 0);
 	this->toolLabel->Font = (gcnew System::Drawing::Font("Microsoft Sans Serif", 10.0f, System::Drawing::FontStyle::Bold));
 
@@ -49,8 +50,6 @@ void MainWindow::CreateToolPanel()
 
 	this->dataPanel->ResumeLayout(true);
 	this->ResumeLayout(true);
-
-	m_graphics->TogglePicking();
 }
 
 void MainWindow::TogglePicking_Clicked(System::Object^ sender, System::EventArgs^ e)
